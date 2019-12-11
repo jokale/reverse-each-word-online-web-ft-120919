bf76cc0b786e447e0bc0dadd1b3693027b525c1d
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 18:47:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
-def reverse_each_word(string)
-  string_new = string.split
-  string_new.each do |string_new|
-    puts "#{string_new.reverse}"
-  end
-  string_new
-end
+
 
 def reverse_each_word(string)
 new_array = []
